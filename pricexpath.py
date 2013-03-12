2e9d310548e1497264a4e4ac476515e89ac44d0d
XiaoyuShan <xys0311@gmail.com>
2013-03-12 16:08:47 -0400

    get price
    use Regular expression to get price xpath.
    Use xpath to read price of product

--- pricexpath.py
+++ pricexpath.py
@@ -5,6 +5,7 @@ import lxml.etree as etree
 import difflib
 from pprint import pprint
 from difflib import SequenceMatcher
+import  re
 
 
 
@@ -25,9 +26,8 @@ def xpath_imgrequest(imgurl, weburl):
         imgresults.append(htree.getpath(Elem_location))
 
     for result in imgresults:
-        print "image Xpath:"+ result
+        #print "image Xpath:"+ result
         imgxpath = result
-
         return imgxpath
 
 def xpath_request(keyword,weburl):
@@ -38,11 +38,14 @@ def xpath_request(keyword,weburl):
 
     hdoc = HTML.fromstring(s)
     htree = etree.ElementTree(hdoc)
-    locations = htree.xpath('//*[text()="'+keyword+'"]')
+    locations = htree.xpath("//*[re:match(text(),'"+keyword+"')]", namespaces={"re": "http://exslt.org/regular-expressions"})
+    #locations = htree.xpath("//*[re:match(text(),'Euro')]", namespaces={"re": "http://exslt.org/regular-expressions"})
+    
+
     results=[]
     for Elem_location in locations:
         results.append(htree.getpath(Elem_location))
-
+        
     for result in results:
         return results
     
@@ -50,29 +53,55 @@ def xpath_request(keyword,weburl):
  
 def xpath_compare(imagexpath, canxpath=[]):
     closest=0
+
     for xpath in canxpath:
+        
         s = difflib.SequenceMatcher(None, imagexpath, xpath)
         matchfirst = s.get_matching_blocks()[0]
         close_degree = matchfirst[2]
+        #print "close degree: ", close_degree
         if closest < close_degree:
             closest = close_degree
             truexpath = xpath
-
     return truexpath
+            
+def info_request(weburl,xpath):
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookielib.CookieJar()))
+    f = opener.open(weburl)
+    s = f.read()
+    f.close()
 
+    hdoc = HTML.fromstring(s)
+    htree = etree.ElementTree(hdoc)
+    
+    locations = htree.xpath(xpath)
+    results=[]
+    for Elem_location in locations:
+        results.append(htree.getpath(Elem_location))
+        print Elem_location.text
+        print Elem_location.attrib
+    for res in results:
+        print res
+        
+        
+    
+    
+    
         
 
    
-imgurl="http://images.manufactum.de/manufactum/thumbs_188/27256_1.jpg"
-weburl="http://www.manufactum.com/trousers-skirts-robes-c193632/"
-keyword = "availability"
+imgurl="http://images.manufactum.de/manufactum/thumbs_188/84498_1.jpg"
+weburl="http://www.manufactum.com/sweaters-c193633/"
+#keyword = "availability"
+keyword = "Euro"
 imgxpath = xpath_imgrequest(imgurl,weburl)
-#print imgxpath
+print "image Xpath:  "+ imgxpath
 sta=[]
 sta=xpath_request(keyword,weburl)
 #for status in sta:
 #    print status
+xpath = xpath_compare(imgxpath, sta)
+print info_request(weburl, xpath)
 
-print keyword + "  XPath:", xpath_compare(imgxpath, sta)
 
 
