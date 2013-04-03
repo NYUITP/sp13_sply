f63ccc516047dad27ceef22c99cad8996d31a687
Zheng Qin <zhengqin@172-29-31-38.dynapool.nyu.edu>
2013-04-03 17:04:53 -0400

    use a single file "bottle_runner" to run the whole project

--- listpage.py
+++ listpage.py
@@ -6,28 +6,30 @@ from difflib import SequenceMatcher
 import urllib2
 import cookielib
 from pprint import pprint
-import  re
+import re
+
+
 @route('/xpath/<weburl:path>,<imgurl:path>')
-def xpath(weburl,imgurl):
-    keywordlist=['Euro','USD','$','RMB']
-    statuslist=['in stock','available']
+def xpath(weburl, imgurl):
+    keywordlist = ['Euro', 'USD', '$', 'RMB']
+    statuslist = ['in stock', 'available']
     kclosest = 0
     sclosest = 0
-    imgresults=[]
-    keyresults=[]
-    statusresults=[]
+    imgresults = []
+    keyresults = []
+    statusresults = []
     imgxpath = ""
-    matchfirst=[]
+    matchfirst = []
     truexpath = ""
-    results=[]
-    price=[]
-    finalstatus=[]
-    currency=""
-    productprice=""
-    productstatus=""
+    results = []
+    price = []
+    finalstatus = []
+    currency = ""
+    productprice = ""
+    productstatus = ""
     opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookielib.CookieJar()))
     weburl = "http://"+weburl
-   
+
     f = opener.open(weburl)
     s = f.read()
     f.close()
@@ -36,62 +38,67 @@ def xpath(weburl,imgurl):
     htree = etree.ElementTree(hdoc)
     imgurl = "http://"+imgurl
     print "imgurl:"+imgurl
+<<<<<<< HEAD
     image_locations = htree.xpath('//img[@src="'+imgurl+'"]')
     
     
    
     if len(image_locations)==False:
+=======
+    # imgurl="http://images.manufactum.de/manufactum/thumbs_188/27532_1.jpg"
+    image_locations = htree.xpath('//img[@src="'+imgurl+'"]')
+    # image_locations = htree.xpath('//img[@src="http://img3.etsystatic.com/000/0/5815299/il_170x135.345221667.jpg"]')
+
+    if len(image_locations) == False:
+>>>>>>> use a single file "bottle_runner" to run the whole project
         print "no image"
-    
-   
+
     for Elem_location in image_locations:
         imgresults.append(htree.getpath(Elem_location))
     for result in imgresults:
         imgxpath = result
         print imgxpath
-        
-    
+
     for keyword in keywordlist:
-        key_locations = htree.xpath("//*[re:match(text(),'"+keyword+"')]", namespaces={"re": "http://exslt.org/regular-expressions"})
+        key_locations = htree.xpath("//*[re:match(text(),'"+keyword+"')]", namespaces={
+                                    "re": "http://exslt.org/regular-expressions"})
         for Elem_location in key_locations:
             keyresults.append(htree.getpath(Elem_location))
-        if len(keyresults)!= False:
+        if len(keyresults) != False:
             currency = keyword
-            break          
-          
+            break
+
     for result in keyresults:
-        #keyxpath = result
-        s = difflib.SequenceMatcher(None,imgxpath, result)
+        # keyxpath = result
+        s = difflib.SequenceMatcher(None, imgxpath, result)
         matchfirst = s.get_matching_blocks()[0]
         close_degree = matchfirst[2]
         if kclosest < close_degree:
             kclosest = close_degree
             truexpath = result
-            
-   
-    
+
     flag = False
     locations = htree.xpath(truexpath)
     for Elem_location in locations:
         results.append(htree.getpath(Elem_location))
         price.append(Elem_location.text)
         pricestr = str(price)
-    pricestr= pricestr[pricestr.find("\'")+1:pricestr.rfind("\'")]
-    if pricestr.isdigit()== True:
-            flag = True
-            productprice = pricestr
-    elif pricestr.isalpha()== True:
-            flag = True
+    pricestr = pricestr[pricestr.find("\'")+1:pricestr.rfind("\'")]
+    if pricestr.isdigit() == True:
+        flag = True
+        productprice = pricestr
+    elif pricestr.isalpha() == True:
+        flag = True
     else:
-           flag = False
-           pricetem=pricestr.split()
-           for tem in pricetem:
-               if tem.find(',') >-1:
-                    productprice=tem
-               elif tem.find('.') >-1:
-                    productprice=tem
-                    break
-        
+        flag = False
+        pricetem = pricestr.split()
+        for tem in pricetem:
+            if tem.find(',') > -1:
+                productprice = tem
+            elif tem.find('.') > -1:
+                productprice = tem
+                break
+
     if flag == True:
         finalxpath = truexpath[:truexpath.rfind('/')]
         finalxpath = finalxpath+'/*'
@@ -103,43 +110,42 @@ def xpath(weburl,imgurl):
         for tem in pricetem:
             tem = tem[tem.find("\'")+1:tem.rfind("\'")]
             print tem
-            if tem.isalpha()==False and tem.find(".")>-1:
-                productprice=tem
+            if tem.isalpha() == False and tem.find(".") > -1:
+                productprice = tem
                 break
-            elif  tem.isalpha()==False and tem.find(",")>-1:
-                productprice=tem
+            elif tem.isalpha() == False and tem.find(",") > -1:
+                productprice = tem
                 break
-       
+
     for status in statuslist:
-        status_locations = htree.xpath("//*[re:match(text(),'"+status+"')]", namespaces={"re": "http://exslt.org/regular-expressions"})
+        status_locations = htree.xpath("//*[re:match(text(),'"+status+"')]", namespaces={
+                                       "re": "http://exslt.org/regular-expressions"})
         for Elem_location in status_locations:
             statusresults.append(htree.getpath(Elem_location))
-        if len(statusresults)!=False:
+        if len(statusresults) != False:
             productstatus = status
             break
     sxpath = ""
     for result in statusresults:
         status_xpath = result
-        s = difflib.SequenceMatcher(None,imgxpath, status_xpath)
+        s = difflib.SequenceMatcher(None, imgxpath, status_xpath)
         matchfirst = s.get_matching_blocks()[0]
         close_degree = matchfirst[2]
         if sclosest < close_degree:
             sclosest = close_degree
             sxpath = status_xpath
-            
-    if len(sxpath)!=False:
+
+    if len(sxpath) != False:
         statuslocations = htree.xpath(sxpath)
         for Elem_location in statuslocations:
             finalstatus.append(Elem_location.text)
-            
+
     else:
         print "can't get status"
-        finalstatus=""
-        
-    return "currency: " + currency + " price: "+productprice +" status:"+ productstatus
-    #return  productprice+productstatus
-    
-    
-run (host='localhost',port = 8081, debug = True)
+        finalstatus = ""
+
+    return "currency: " + currency + " price: "+productprice + " status:" + productstatus
+    # return  productprice+productstatus
 
 
+run(host='localhost', port=8082, debug=True)
---
+++ schemaBottle.py
@@ -0,0 +1,133 @@
+#!/usr/local/bin python
+# coding=utf-8
+
+from bottle import route, run
+from pprint import pprint
+import urllib2
+import lxml.etree as etree
+import httplib
+import urlparse
+
+
+# @route('/schema/<url:path>')
+
+# def schema(url):
+        # if is_url_available(url):
+        # 	page = urlreader(url)
+        # 	if is_url_schema(page):
+        # 		print (get_product_name(page) + " is " + get_product_price(page))
+        # print get_product_price(url)
+        # 		print " "
+        # 	else:
+        # 		print ("***"+weburl+"***  ")
+        # 		print "This is not a Schema type website!"
+        # 		print " "
+        # else:
+        # 	print "URL not exists"
+        # 	print " "
+def schema(url):
+    if is_url_available(url):
+        page = urlreader(url)
+        if is_url_schema(page):
+            if is_url_instock(page):
+                print (get_product_name(page) + " is " + get_product_price(page))
+                print " "
+                # product_name = get_product_name(page)
+                # product_price = get_product_price(page)
+                # return product_name + " is " + product_price
+            elif is_product_onsale(page):
+                print (get_product_name(page) + " is ON SALE!! The on sale price is " + get_onsale_price(page))
+                print " "
+            else:
+                if is_product_onsale(page):
+                    print (get_product_name(page) + " is ON SALE!! The on sale price is " + get_onsale_price(page))
+                    print " "
+                    # product_name = get_product_name(page)
+                    # onsale_price = get_onsale_price(page)
+                    # return product_name + " is ON SALE!! The on sale price is " + onsale_price
+                else:
+                    print (get_product_name(page)+" is out of stock!")
+                    print " "
+                    # product_name = get_product_name(page)
+                    # return product_name + " is out of stock!"
+        else:
+            print ("***"+url+"***  ")
+            print "This is not a Schema type website!"
+            print " "
+            # return "This is not a Schema type website!"
+    else:
+        print "URL not exists"
+        print " "
+        # return "URL not exists"
+
+
+def urlreader(url):
+    page_html = urllib2.urlopen(url).read()
+    page = etree.HTML(page_html.lower())
+    return page
+
+
+def is_url_available(url):
+    host, path = urlparse.urlsplit(url)[1:3]
+    found = 0
+    try:
+        connection = httplib.HTTPConnection(host)  # Make HTTPConnection Object
+        connection.request("HEAD", path)
+        responseOb = connection.getresponse()  # Grab HTTPResponse Object
+
+        if responseOb.status == 200:
+            found = 1
+        else:
+            print "Status %d %s : %s" % (responseOb.status, responseOb.reason, url)
+    except Exception, e:
+        print e.__class__,  e, url
+    return found
+
+
+def is_url_schema(page):
+    flag = page.xpath('//*[@itemprop="name"]')
+    result = 1
+    if (flag == []):
+        result = 0
+    return result
+
+
+def is_url_instock(page):
+    flag = page.xpath('//*[@itemprop="price"]')
+    result = 1
+    if (flag == []):
+        result = 0
+    return result
+
+
+def is_product_onsale(page):
+    flag = page.xpath('//*[@id="ourprice"]')
+    result = 1
+    if (flag == []):
+        result = 0
+    return result
+
+
+def get_onsale_price(page):
+    prices = page.xpath('//*[@id="ourprice"]')
+    price = prices[0]
+    return price.text.strip()
+
+
+def get_product_price(page):
+    prices = page.xpath('//*[@itemprop="price"]')
+    price = prices[0]
+    return price.text.strip()
+
+
+def get_product_name(page):
+    names = page.xpath('//*[@itemprop ="name"]')
+    name = names[0]
+    return name.text.strip()
+
+
+def helloworld():
+    return "Hello World!"
+
+
+# run(host='localhost', port=8082, debug=True)
