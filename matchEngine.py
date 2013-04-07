2b3d9dd4009dcdbedfa83fd19fa990d64299f396
Zheng Qin <zhengqin@172-29-30-191.dynapool.nyu.edu>
2013-04-07 16:41:19 -0400

    delete old get price file

--- matchEngine.py
+++ matchEngine.py
@@ -1,22 +1,17 @@
 import get_price
 import get_all_prices_xpath
 
-# urls = {
-#     'http://www.barnesandnoble.com/p/toys-games-five-crowns-card-game/18360665?ean=9780963469144&isbn=9780963469144'
-# }
+
 file = open("web_urls.txt")
 
 while 1:
-    lines = file.readlines(100000)
-    if not lines:
+    urls = file.readlines(100000)
+    if not urls:
         break
-    for line in lines:
-        # print line,
-        url = line,
-        get_price.get_price(url)
-        get_all_prices_xpath.get_price(url)
+    for url in urls:
+        print url,
+        get_price.get_price(url,)
+        get_all_prices_xpath.get_price(url,)
+file.close()
 
 
-# for url in open("web_urls.txt"):
-#     get_price.get_price(url)
-#     get_all_prices_xpath.get_price(url)
