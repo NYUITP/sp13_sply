175ff16d8aa2879292b1e5a2b48bedb3285b41d1
Zheng Qin <zhengqin@172-26-204-156.dynapool.nyu.edu>
2013-04-07 17:23:49 -0400

    Up to date of get price from Xiaoyu

--- get_price.py
+++ get_price.py
@@ -14,6 +14,8 @@ import re
 
 def get_price(weburl):
     final = []
+    keyresults = []
+    truexpath = ""
     opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookielib.CookieJar()))
     f = opener.open(weburl)
     s = f.read()
@@ -22,53 +24,94 @@ def get_price(weburl):
     hdoc = HTML.fromstring(s)
     htree = etree.ElementTree(hdoc)
     # search xpath contain currency symbol
-    key = xpath_filter()
-    key_locations = htree.xpath("//*[re:match(text(),'"+key+"')]", namespaces={"re": "http://exslt.org/regular-expressions"})
-    keyresults = []
-    truexpath = ""
-    currency = ""
-    for Elem_location in key_locations:
-        keyresults.append(htree.getpath(Elem_location))
+    keys = xpath_filter()
+    for key in keys:
+        key_locations = htree.xpath("//*[re:match(text(),'"+key+"')]", namespaces={
+                                    "re": "http://exslt.org/regular-expressions"})
+        for Elem_location in key_locations:
+            keyresults.append(htree.getpath(Elem_location))
+        if len(keyresults) != False:
+            break
 
     # get price and currency
 
     for result in keyresults:
         truexpath = result
+        final_currency = ""
+        final_price = ""
+        final_symbol = ""
         if truexpath.find('script') == -1:
-            # print truexpath
+            print truexpath
+            modxpath = []
+            modxpath.append(truexpath[:truexpath.rfind('/')])
+            modxpath.append(truexpath[:truexpath.rfind('/')+1]+"*")
+            # print modxpath
             locations = htree.xpath(truexpath)
-            # truexpath= truexpath[truexpath.find("\'")+1:truexpath.rfind("\'")]
             for Elem_location in locations:
                 price = Elem_location.text
                 if price != None:
+                    final_currency = currency_get(price)
+                    final_symbol = currency_get(price)
                     flag = price_check(price)
                     # print flag
                     if flag == False:
-                        final.append(price_get(price, truexpath))
+                        final_price = price_get(price)
                     else:
-                        truexpath = truexpath[:truexpath.rfind('/')+1]+"*"
-                        locations = htree.xpath(truexpath)
-                        # locations = htree.xpath("/html/body/div[2]/section/section/div[2]/p[1]/span")
+                        break
+            if flag == True:
+                for mod in modxpath:
+                    pricestr = ""
+                    loc = htree.xpath(mod)
+                    for Elem_location in loc:
+                        price = Elem_location.text
+                        pricestr = pricestr + " "+price
+                        if price_get(pricestr) != "No price":
+                            final_price = price_get(pricestr)
+                            break
 
-                        for Elem_location in locations:
-                            price = Elem_location.text
-                            if price != None:
-                                final.append(price_get(price, truexpath))
-
-    print final[0]
-    return final
+        print final_currency
+        print final_price
+        print truexpath
+    return (final_currency, final_price, truexpath)
 
 
 def xpath_filter():
+    key = []
     currencyhead = "€|￥|£|$"
     curhead = "[€,￥,£,$]"
     curhead = unicode(curhead, 'utf-8')
     currencyhead = unicode(currencyhead, 'utf-8')
-    currencytail = "EUR|Euro|RMB|RBP|JPY|CAD|AUD|USD"
+    currencytail = "EUR|Euro|RMB|RBP|JPY|AUD|USD"
     numberstring = "\\d+(?:(?:\\.|,)\\d\\d)?"
-    # key = "\\s*?(?:"+currencyhead+")?\\s*"+numberstring+"\\s*(?:"+currencytail+")+?\\s*?"
-    key = "^"+currencyhead+""+numberstring+""+currencytail+"$"
+    key.append("^"+curhead+""+numberstring+""+currencytail+"$")
+    key.append("^"+curhead+""+numberstring+"$")
+    key.append("^"+numberstring+""+currencytail+"$")
+
     return key
+
+
+def currency_get(price):
+    pricetem = price.split()
+    for tem in pricetem:
+        # print tem
+        m1 = re.search('^[0-9\.\,]{1,}$', tem)
+        m2 = re.search('^[A-Za-z]{1,}$', tem)
+        m4 = re.search('^.[0-9\.\,]{1,}$', tem)
+        if m1 != None:
+            price = tem
+        elif m4 != None:
+            price = tem[1:]
+            symbol = tem[:1]
+        elif m2 != None:
+            currency = tem
+        elif m1 == None and m2 == None:
+            symbol = tem
+        else:
+            print "error"
+    if len(currency) > -1:
+        return currency
+    else:
+        return symbol
 
 
 def price_check(price):
@@ -76,6 +119,8 @@ def price_check(price):
     curhead = "[€,￥,£,$]"
     curhead = unicode(curhead, 'utf-8')
     flag = False
+    currency = ""
+    symbol = ""
     reg4 = curhead+"?.[0-9\,\.]{1,}"
     m4 = re.search(reg4, price)
     reg5 = "^[0-9\.\,]{1,}?.("+currencytail+")$"
@@ -99,61 +144,74 @@ def price_check(price):
     if m1 != None:
         flag = True
         # print "m1" + m1.group(0)
+        symbol = price
     elif m2 != None:
         flag = True
         # print "m2" + m2.group(0)
+        currency = price
     elif m3 != None:
         flag = True
         # print "m3" + m3.group(0)
     else:
         # print "some else"
         flag = False
+        symbol = price[:1]
+        currency = price[1:]
         # print price
     return flag
 
 
-def price_get(pri, xpath):
-    currency = ""
-    price = ""
-    symbol = ""
-    strflag = False
-
+def price_get(pri):
+    price = "No price"
     pricetem = pri.split()
+
     for tem in pricetem:
-        print tem
+        # print tem
         m1 = re.search('^[0-9\.\,]{1,}$', tem)
+        m2 = re.search('^[A-Za-z]{1,}$', tem)
+        m4 = re.search('^.[0-9\.\,]{1,}$', tem)
         if m1 != None:
-            strflag = True
             price = tem
-    if strflag == True:
-        for tem in pricetem:
-            m2 = re.search('^[A-Za-z]{1,}$', tem)
-            m3 = re.search('^\$$', tem)
-            if m2 == None:
-                if m3 != None:
-                    symbol = tem
-                # else:
-                    # print "not price"
-            else:
-                currency = tem
-    else:
-        for tem in pricetem:
-            m4 = re.search('^.[0-9\.\,]{1,}$', tem)
-            if m4 != None:
-                symbol = tem[:1]
-                price = tem[1:]
-            print price
+        elif m4 != None:
+            price = tem[1:]
+            symbol = tem[:1]
+        elif m2 != None:
+            currency = tem
+        elif m1 == None and m2 == None:
+            symbol = tem
+        else:
+            print "error"
 
-    return (symbol, currency, price, xpath)
+    return price
 
-
-# url="http://www.oipolloi.com/apc-cable-knit-pullover-off-white",
+    '''
+    url = [ "http://www.jcrew.com/AST/Browse/MensBrowse/Men_Shop_By_Category/shirts/washedfavoriteshirts/PRDOVR~14915/14915.jsp",
+        "http://hypebeast.com/2009/05/xlarge-x-casio-g-shock-dw-5600/",
+        "http://hypebeast.com/2009/05/cheez-2009-summer-new-releases/",
+        "http://www.watchismo.com/diesel-dz4160-freak-of-nature-watch.aspx",
+        "http://hypebeast.com/2009/05/takashi-kumagai-for-quiksilver-checkered-foulards/",
+        "http://men.style.com/news/blog/2009/05/common-projects-strikes-again.html?mbid=rss_upgrdr",
+        "http://www.warpweb.jp/shopdetail/008000000001/order/",
+        "http://rumplo.com/tees/tee/7110-run-buddha-run",
+        "http://rumplo.com/tees/tee/9963-eclectic-method-merch--t shirt--eclectic-method-jetset-remix",
+        "http://alterbrooklyn.blogspot.com/2009/05/new-takel-shirts.html",
+        "http://gizmodo.com/303906/diesel-rolls-out-oled-watches-two handed-time-telling-returns-from-1970s",
+        "http://www.selectism.com/news/2008/12/08/maurice-de-mauriac-chronographs/maurice-de-mauriac-watches-01/",
+        "http://goincase.com/products/detail/CL55060",
+        "http://goincase.com/products/detail/CL55059",
+        "http://amazon.com/dp/B000IXBUEO/?tag=svpply01-20",
+        "http://www.chromebagsstore.com/soyuzblkblk.html",
+        "http://gearpatrol.com/blog/2009/05/08/rocky-roads-custom-vintage-ford-broncos/",
+        "http://www.zappos.com/n/multi_view.cgi?product_id=7419889&color_id=422&view=multi&ref=multi-pic"
+        ]
+    '''
+# url="http://www.oipolloi.com/apc-cable-knit-pullover-off-white"
 # url="http://www.urbanoutfitters.com/urban/catalog/productdetail.jsp?id=26503334&parentid=M_APP_SHORTSSWIM_SHORTS"
 # url="http://www.urbanoutfitters.com/urban/catalog/category.jsp?id=BRANDS&brand=rothco"
 # url="http://www.freepeople.com/whats-new-intimates/"
 # url = "http://www.manufactum.com/sweaters-c193633/"
 # url = "http://www.etsy.com"
-url ="http://www.urbanoutfitters.com/urban/catalog/category.jsp?id=BRANDS&brand=rothco"
+# url ="http://www.urbanoutfitters.com/urban/catalog/category.jsp?id=BRANDS&brand=rothco"
 # url="http://www.zara.com/webapp/wcs/stores/servlet/category/us/en/zara-nam-S2013/358080/Jackets"
 # url="http://www.zara.com/webapp/wcs/stores/servlet/product/us/en/zara-nam-S2013/358082/1224520/POLKA+DOT+DRESS"
-# url = get_price(url)
+# result = get_price(url)
--- matchEngine.py
+++ matchEngine.py
@@ -1,17 +1,15 @@
 import get_price
-import get_all_prices_xpath
 
 
-file = open("web_urls.txt")
-
-while 1:
-    urls = file.readlines(100000)
-    if not urls:
-        break
-    for url in urls:
-        print url,
-        get_price.get_price(url,)
-        get_all_prices_xpath.get_price(url,)
-file.close()
+def check_results(filename):
+    file = open(filename)
+    while 1:
+        urls = file.readlines(100000)
+        if not urls:
+            break
+        for url in urls:
+            get_price.get_price(url,)
+    file.close()
 
 
+check_results('web_urls.txt')
